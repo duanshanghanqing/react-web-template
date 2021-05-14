3db11f5c3492bddeadc45137da1d376802f9b032
高俊峰 <junfeng.gao@ingeek.com>
2021-05-14 17:15:05 +0800

    add

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './index.less';
-import { Input } from 'library';
 
 export default class extends React.Component {
   render() {
     return (
       <div className="Index">
-        <Input />
         <h3>a react web app</h3>
       </div>
     );
