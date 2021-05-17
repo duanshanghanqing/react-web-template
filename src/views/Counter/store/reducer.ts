e038288d6ba6af359b811a523573e2718db5fb64
高俊峰 <junfeng.gao@ingeek.com>
2021-05-17 16:02:54 +0800

    add code

--- src/views/Counter/store/reducer.ts
+++ src/views/Counter/store/reducer.ts
@@ -1,6 +1,5 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './actions';
 
-// reducer其实也是个方法而已,参数是state和action,返回值是新的state
 export default function (state = 0, action) {
   switch (action.type) {
     case INCREMENT_COUNTER:
